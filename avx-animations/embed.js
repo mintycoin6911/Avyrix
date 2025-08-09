1dbe7ad1b017739596edfd9c5b2db5c0101c404c
mintycoin6911 <mintycoin6911@gmail.com>
2025-08-09 12:04:29 -0700

    Update embed.js

--- avx-animations/embed.js
+++ avx-animations/embed.js
@@ -1,1 +1,11 @@
-console.log('AVX animations loaded');
+<section style="margin-top:40px">
+  <h2>🥚 Egg Hatch Demo</h2>
+  <iframe src="./avx-animations/egg-hatch.html" width="100%" height="520" style="border:none;border-radius:12px; display:block;"></iframe>
+  <p><a href="./avx-animations/egg-hatch.html" target="_blank">Open Egg Hatch in a new tab</a></p>
+</section>
+
+<section style="margin-top:40px">
+  <h2>🪽 Evolution Demo</h2>
+  <iframe src="./avx-animations/evolution.html" width="100%" height="760" style="border:none;border-radius:12px; display:block;"></iframe>
+  <p><a href="./avx-animations/evolution.html" target="_blank">Open Evolution in a new tab</a></p>
+</section>
